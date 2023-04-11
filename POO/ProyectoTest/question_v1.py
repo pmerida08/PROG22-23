2feb853c454d8de0fdeed19922d1c003006a114e
pmerida08 <a21mevepa@iesgrancapitan.org>
2023-04-11 18:34:26 +0200

    corrigiendo v1 Question de Proyecto

--- POO/ProyectoTest/question_v1.py
+++ POO/ProyectoTest/question_v1.py
@@ -1,13 +1,13 @@
 class Question:
     __BASE_POINTS_MAX = 2
-    __number_quests = 0 # Solo sirve para el método show
+    __number_quests = 0
 
-    def __init__(self, name: str, enunciate: str, elections: list[tuple[str, float]]):
+    def __init__(self, name: str, enunciate: str, elections: list[tuple[str, float]], base_pointed: int):
         self.__name = name
         self.__enunciate = enunciate
         self.__elections = elections
+        self.__base_pointed = base_pointed
         Question.__number_quests += 1
-
     @property
     def enunciate(self):
         return self.__enunciate
@@ -21,17 +21,26 @@ class Question:
         if election == '':
             print(f'La puntuación obtenida es de 0 puntos')
         for option, points in self.__elections:
-            if option == election:
-                print(f'La puntuación obtenida es de {points} puntos')
+            if option != election:
+                print(f'La puntuación obtenida es de {self.__base_pointed*points} puntos')
+                break
+            elif option == election:
+                print(f'La puntuación obtenida es de {self.__base_pointed*points} puntos')
+                break
 
     def show(self):
-        for i in range(Question.__number_quests):
-            print(f'Pregunta {i+1}. {self.__enunciate}')
+        print(f'Pregunta {Question.__number_quests}. {self.__enunciate}')
         for election in self.__elections:
             print(f"- {election[0]}")
 
 if __name__ == '__main__':
     q1 = Question('q1', '¿Qué son los mamíferos?',
-                [('Seres vivos', 2), ('Plantas', -0.25), ('Insectos', -0.25), ('Partido político', -0.25)])
+                [('Seres vivos', 2), ('Plantas', -0.25), ('Insectos', -0.25), ('Partido político', -0.25)], 1)
     q1.show()
     q1.answer()
+
+    q2 = Question('q2', '¿En qué nivel evoluciona Totodile en Croconaw?',
+                [('16', -0.25), ('18', 2), ('17', -0.25), ('19', -0.25)], 1)
+    q2.show()
+    q2.answer()
+
