60dd23c124cb2dfc4207eb350e86dfedb1cc19f7
pmerida08 <a21mevepa@iesgrancapitan.org>
2023-04-18 09:26:25 +0200

    question_v3 json

--- POO/ProyectoTest/question_v3_json.py
+++ POO/ProyectoTest/question_v3_json.py
@@ -1,21 +1,32 @@
 import json
+from dataclasses import dataclass
+from json import JSONDecoder
+from typeguard import typechecked
 
-__BASE_POINTS_MAX = 2
-__number_quests = 0
+JSON_FILE = "questions.json"
 
-def show():
-    count = 0
-    with open('questions.json', 'rt', encoding='utf-8') as json_file:
-        data = json.load(json_file)
+@typechecked
+@dataclass(frozen=True)
+class Question:
+    name: str
+    statement: str
+    options: list[tuple[str, float]]
+    base_pointed: int
 
-        quest = data[count]
-        name = quest['name']
-        statement = quest['statement']
-        options = quest['options']
-        points = quest['base_pointed']
-
-    print(f'{name}{count + 1}: {statement}')
-
+class QuestionEncoder(JSONDecoder):
+    def default(self, dec):
+        return dec.__dict__
 
 if __name__ == '__main__':
-    show()
+    questions = [
+        Question('q1', '¿Qué son los mamíferos?',[('Seres vivos', 2), ('Plantas', -0.25), ('Insectos', -0.25), ('Partido político', -0.25)], 1),
+        Question('q2', '¿En qué meridiano está España?',[('M. magnético', -0.25), ('M. de Greenwich', 2), ('M. cero', -0.25), ('Ecuador', -0.25)], 1),
+        Question('q3', '¿Cuál es la capital de Marruecos?',[('Ceuta', -0.25), ('Melilla', -0.25), ('Rabat', 2), ('Marrakech', -0.25)], 1),
+        Question('q4', '¿En qué año murió Francisco Franco?',[('1976', -0.25), ('1973', -0.25), ('1978', -0.25), ('1975', 2)], 1),
+        Question('q5', '¿Cuál fue el primer programador/a?',[('Ada Lovelace', 2), ('von Neumann', -0.25), ('Peter Griffin', -0.25), ('Bill Gates', -0.25)], 1)
+    ]
+
+    with open(JSON_FILE, "wt") as file:
+        json.dump(questions, file, ensure_ascii=False, indent=4, cls=QuestionEncoder)
+
+    print(f'Creado {JSON_FILE}')
